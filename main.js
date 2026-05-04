a7ecab8df7ce2c8087dca3c0965090eab25d4d5c
Sherry Zhu <sherryzhu@Sherrys-MacBook-Air.local>
2026-05-04 13:42:07 -0600

    Rebuild private voice call flow

--- main.js
+++ main.js
@@ -52,11 +52,11 @@ const OFFLINE_INVITE_FOCUS_KEY = 'offline_invite_focus_id_v1';
 const OFFLINE_INVITE_REMINDER_SNOOZE_MS = 15 * 60 * 1000;
 const BACKEND_LOG_STORAGE_KEY = 'backend_runtime_logs_v1';
 const BACKEND_LOG_MAX = 1000;
-const APP_BUILD_ID = '2026-05-04T15:49:47Z';
+const APP_BUILD_ID = '2026-05-04T19:25:33Z';
 const APP_UPDATE_NOTES = [
-  '重做通话页黑白样式',
-  '通话输入框跟随键盘',
-  '减少通话页拥挤控件'
+  '重做通话输入逻辑',
+  '精简通话页界面',
+  '修正通话键盘跟随'
 ];
 const HOME_WIDGET_MINI_ORB_KEY = 'home_widget_mini_orb_image';
 const HOME_CLOCK_WIDGET_ART_KEY = 'home_clock_widget_art';
